4fc8eadf510db4e51ebb5766c9df28fb06b6d648
Seokwoo <swoo1429@naver.com>
2022-06-17 15:55:00 +0900

    push

---
+++ netslice/src/api/main.js
@@ -0,0 +1,16 @@
+import api from "./core/index";
+
+const apis = {
+    // getComments : ({board_id})=> api.get(`/api/detail/${board_id}/reply`),
+    // postComment : (payload)=> api.post(`/api/detail/${payload.board_id}/reply`,{
+    //     reply_text: payload.reply_text,
+    // }),
+    // deleteComment : (payload)=> api.delete(`/api/detail/${payload.board_id}/${payload.reply_id}`),
+    // putComment : (payload)=> api.put(`/api/detail/${payload.board_id}/${payload.reply_id}`,{
+    //     reply_text: payload.reply_text,
+    // }),
+    // getRank : ()=>api.get(`/api/best`),
+    // deleteComments : (payload)=>api.delete(`/api/detail/${payload.board_id}/reply`)
+}
+
+export default apis;
