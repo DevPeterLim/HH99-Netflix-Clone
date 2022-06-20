872642192f3cfe4f8ffcfd12f7dd3dfea232727f
DevPeterLim <98936928+DevPeterLim@users.noreply.github.com>
2022-06-20 09:52:14 +0900

    routing in App.js

--- netslice/src/App.js
+++ netslice/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeBeforeLogin from './page/HomeBeforeLogin';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Signup from './page/Signup';
+import Login from './page/login';
+
 
 function App() {
   return (
     <div className="App">
-     <HomeBeforeLogin></HomeBeforeLogin>
+      <Routes>
+        <Route path='/' element={<HomeBeforeLogin/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/login' element={<Login/>} />
+      </Routes>
+
     </div>
   );
 }
